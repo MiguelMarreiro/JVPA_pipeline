474a2d606356f0498273c9cd929a32893b6e3b17
MiguelMarreiro <miguel.marreiro@gmail.com>
2026-02-11 19:28:00 +0000

    fix bug when there is no footer

--- doc_processor/word_processor_streamlit_multi.py
+++ doc_processor/word_processor_streamlit_multi.py
@@ -125,7 +125,7 @@ if __name__ == "__main__":
         for i, article in enumerate(articles, 1):
             st.subheader(f"Artigo {i}")
             st.text_area(f"{article["Titulo"]}", article["BODY"], height=200)
-            if article["Rodape"] != "":
+            if article["Rodape"]:
                 st.text_area("Rodapé", article["Rodape"], height=60)
 
 
